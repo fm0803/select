ccd494a2eb731b3dfd5b2e1b353f34eabbfad840
fangman <fangman@sensetime.com>
2020-11-27 19:41:35 +0800

    select

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	 "time"
+)
+
+func main() {
+	ch := make (chan int,10)
+	go func() {
+		// do something
+		time.Sleep(time.Second*3) // sleep one second
+		ch <- 1
+	}()
+	select {
+	case <- ch:
+	case <- time.After(time.Second * 1):
+		fmt.Println("timeout")
+	}
+}
